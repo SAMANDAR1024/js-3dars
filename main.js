61d87ce375c9c8165879155c099174e84c3b6b40
Samandar <zafarovsamandar5444@gmail.com>
2024-10-16 15:48:26 +0500

    12345678

---
+++ main.js
@@ -0,0 +1,331 @@
+// let a = prompt("Kamissia yilini kiriting");
+// if(a % 4 == 0){
+//     alert("kamissia yili");
+// }else{
+//     alert("kamisia yili emas");
+// }
+
+// let a = prompt("temperaturasini kiriting");
+// if(a < 0){
+//     alert("temperaturasi juda pas");
+// }else if(a >0 && a <10){
+//     alert("temperaturasi pas");
+// }else if(a >= 10 && a < 16){
+//     alert("temperaturasi norm");
+// }else if(a >= 16 && a < 20){
+//     alert("temperaturasi azgina yaxwi");
+// }else if(a >= 20 && a < 30){
+//     alert("temperaturasi baland");
+// }else if(a >= 30 && a < 50){
+//     alert("temperaturasi juda baland");
+// }else{
+//     alert("temperaturasi slishkm baland");
+// }
+
+// let a = prompt("son kiriting");
+// if( a % 2==0 && a % 3==0){
+//     alert("true");
+// }else{
+//     alert("false")
+// }
+
+// let a = prompt("meva kiriting");
+// switch (a) {
+//   case "olma":
+//     alert("bu mevaning narxi 10.000");
+//     break;
+//   case "nok":
+//     alert("bu mevaning narxi 13.000");
+//     break;
+//   case "uzum":
+//     alert("bu mevaning narxi 15.000");
+//     break;
+//   case "shoftoli":
+//     alert("bu mevaning narxi 18.000");
+//     break;
+
+//   default:
+//     alert("bunaqa meva yoq");
+//     break;
+// // }
+
+// let a= prompt("son kiriting");
+// switch (a) {
+//     case "1":
+//         alert("Breakfast");
+//         break;
+//         case "2":
+//         alert("Lunch");
+//         break;
+//         case "3":
+//         alert("Dinner");
+//         break;
+//         case "4":
+//         alert("Snack");
+//         break;
+
+//     default:
+//         alert("Bunday narsa yoq qayta harakat qb korin oxshab qolar");
+//         break;
+// }
+
+// let a=prompt("1-son kiriting");
+// let b=prompt("2-son kiriting");
+// if(a>b){
+//     alert(a);
+//     alert(b);
+// }else{
+//     alert(b);
+//     alert(a);
+// }
+
+// 1-MISOL👇
+
+// let a=prompt("SON KIRITING");
+// if(a==parseInt(a)){
+//     if(a>0){
+//         alert(++a);
+//     }else{
+//         alert(a);
+//     }
+// }else{
+//     alert("butun son kiritin");
+
+// }
+
+// 2-MISOL👇
+
+// let a=prompt("SON KIRITING");
+// if(a==parseInt(a)){
+//     if(a>0){
+//         alert(++a);
+//     }else{
+//         alert(a-2);
+//     }
+
+// }else{
+//     alert(" butun son kiritin");
+
+// }
+
+// 3-MISOL👇
+
+// let a=prompt("SON KIRITING");
+// if(a==parseInt(a)){
+//     if(a>0){
+//         alert(++a);
+//     }else if(a<0){
+//         alert(a-2);
+
+//     } else if(a==0){
+//         alert("10");
+//     }
+
+// }
+// else{
+//     alert(" butun son kiritin");
+
+// }
+
+// 4-MISOL👇
+// let a = prompt("a sonini kiritng!");
+// let b = prompt("b sonini kiritng!");
+// let c = prompt("c sonini kiritng!");
+
+// let count = 0;
+
+// if (a > 0) count++;
+// if (b > 0) count++;
+// if (c > 0) count++;
+
+// alert("kiritilgan sonlaari ichida " + count + " tasi musbat");
+
+// 5-MISOL👇
+// let a = prompt("a sonini kiritng!");
+// let b = prompt("b sonini kiritng!");
+// let c = prompt("c sonini kiritng!");
+
+// let count = 0;
+// let countt = 0;
+
+// if (a > 0) count++;
+// if (b > 0) count++;
+// if (c > 0) count++;
+// if (a < 0) countt++;
+// if (b < 0) countt++;
+// if (c < 0) countt++;
+
+// alert("kiritilgan sonlaari ichida " + count + " tasi musbat");
+// alert("kiritilgan sonlaari ichida " + countt + " tasi manfiy");
+
+
+
+
+// 6-MISOL👇
+// let a = prompt("a sonini kiritng!");
+// let b = prompt("b sonini kiritng!");
+
+// if (a > b) {
+//   alert(a);
+// } else {
+//   alert(b);
+// }
+
+
+
+
+// // 7-MISOL👇
+// let a = +prompt("a sonini kiritng!");
+// let b = +prompt("b sonini kiritng!");
+
+// if (a > b) {
+//   alert(`Kichik sonning tartib raqami ` + 2);
+// }else {
+//   alert(`Kichik sonning tartib raqami ` + 1);
+// }
+
+
+ 
+
+// // 8-MISOL👇
+// let a = prompt("a sonini kiritng!");
+// let b = prompt("b sonini kiritng!");
+
+// if (a > b) {
+//   alert(` kaattasi ${a} + kichigi ${b}`)
+// } else {
+//     alert(` kaattasi ${b} + kichigi ${a}`)
+// }
+
+
+
+// // 9-MISOL👇
+// let a = +prompt("a sonini kiritng!");
+// let b = +prompt("b sonini kiritng!");
+
+// if (a > b){
+//    let box = b;
+//    b = a;
+//    a = box;
+//    alert( " B soni = "  + b + " A soni = " + a  );
+// }else{
+//     alert( " B soni =" + b + " A soni = " + a );
+// }
+
+
+
+// // 10-MISOL👇
+// let a = +prompt("a sonini kiritng!");
+// let b = +prompt("b sonini kiritng!");
+//  if(a != b ){
+//     alert(a + b);
+//  }else {
+//     alert(" A va B  0 ga teng ")
+//  }
+
+
+
+
+
+
+// // 11-MISOL👇
+// let a = +prompt("a sonini kiritng!");
+// let b = +prompt("b sonini kiritng!");
+//  if(a > b ){
+//     alert( " A = " + a + " B = " + a );
+//  }else if(a < b) {
+//     alert(" A = " + b + " B = " + b)
+//  }else{
+//      alert( " a = 0 , b = 0 " )
+//  }
+
+
+// a > b && a > c 
+
+// // 12-MISOL👇
+// let a = +prompt("a sonini kiritng!");
+// let b = +prompt("b sonini kiritng!");
+// let c = +prompt("b sonini kiritng!");
+
+// if( a < b && a < c ){
+//     alert(a);
+// }else if( b < a && b < c ){
+//     alert(b);
+// }else {
+//     alert(c);
+// }
+
+
+
+
+// // 13-MISOL👇
+// let a = +prompt("a sonini kiritng!");
+// let b = +prompt("b sonini kiritng!");
+// let c = +prompt("c sonini kiritng!");
+
+// if( a < b && a > c || a > b && a < c ){
+//     alert(a);
+// }else if( b < a && b > c || b > a && b < c ){
+//     alert(b);
+// }else {
+//     alert(c);
+// }
+
+
+
+
+
+
+// // 14-MISOL👇
+// let a = +prompt("a sonini kiritng!");
+// let b = +prompt("b sonini kiritng!");
+// let c = +prompt("c sonini kiritng!");
+
+// if( a < b && a < c ){
+//     alert(a);
+// }else if( b < a && b < c ){
+//     alert(b);
+// }else {
+//     alert(c);
+// }    
+
+
+// if( a > b && a > c ){
+//     alert(a);
+// }else if( b > a && b > c ){
+//     alert(b);
+// }else {
+//     alert(c);
+// }
+
+
+
+
+
+
+// // 15-MISOL👇
+
+let a = +prompt("a sonini kiritng!");
+let b = +prompt("b sonini kiritng!");
+let c = +prompt("c sonini kiritng!");
+
+let ort;
+let max;
+
+if( a < b && a > c || a > b && a < c ){
+    ort = a;
+}else if( b < a && b > c || b > a && b < c ){
+    ort =b;
+}else {
+    ort = c;
+}
+
+if( a > b && a > c ){
+    max = a
+}else if( b > a && b > c ){
+    max = b;
+}else {
+    max = c;
+}
+
+alert(`Maximum ${max} va O'rtacha ${ort}, shu sonlarning yi'gindisi eng kotta bo'ladi`)
